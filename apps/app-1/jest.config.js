3f3505a0fffd693f1556c39f83214612a7b6f506
DriLLFreAK100 <blusky_8893@hotmail.com>
2022-01-14 21:21:55 +0800

    scaffold nx workspace

---
+++ apps/app-1/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'app-1',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/app-1',
+};
---
+++ apps/app-2/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'app-2',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/app-2',
+};
